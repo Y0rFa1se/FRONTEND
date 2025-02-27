95a1d95ae9e600be9853c9d590e88364fae999f0
Y0rFa1se <neargul123@gmail.com>
2025-02-28 04:03:47 +0900

    asdf

--- src/routes/apps/stock_database/+page.server.ts
+++ src/routes/apps/stock_database/+page.server.ts
@@ -1,5 +1,5 @@
 export async function load() {
-    const response = await fetch("http://127.0.0.1:8000/database/stock/tickers");
+    const response = await fetch("http://192.168.0.18:8000/database/stock/tickers");
     const tickers = await response.json();
 
     return { data: tickers };
--- src/routes/apps/stock_database/[ticker]/+page.server.ts
+++ src/routes/apps/stock_database/[ticker]/+page.server.ts
@@ -1,7 +1,7 @@
 export async function load({ params }) {
     const ticker = params.ticker;
 
-    const response = await fetch("http://127.0.0.1:8000/database/stock/prices/" + ticker);
+    const response = await fetch("http://192.168.0.18:8000/database/stock/prices/" + ticker);
     const prices = await response.json();
 
     console.log(prices);
