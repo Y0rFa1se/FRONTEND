51f2f4e6ed91bb6dc83d2d9cb9e737bb049ef5ba
Y0rFa1se <neargul123@gmail.com>
2025-03-08 10:46:02 +0900

    asdf

---
+++ src/routes/apps/+layout.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+import { page } from "$app/state";
+    import { goto } from "$app/navigation";
+
+    const ticker = page.data.data;
+    console.log(ticker);
+
+    const urls: string[] = [
+        "https://y0rfa1se.duckdns.org/api/database/session/check",
+    ];
+
+    // 쿠키에서 세션 ID 가져오기
+    const cookies = document.cookie;
+    console.log("Cookies:", cookies);
+
+    const sessionCookie = cookies
+        .split(";")
+        .find((cookie) => cookie.trim().startsWith("session_id="));
+    const sessionId = sessionCookie?.split("=")[1];
+
+    console.log("Session ID:", sessionId);
+
+    // 비동기 함수로 API 호출 및 처리
+    async function checkSessions() {
+        if (!sessionId) {
+            console.error("Session ID not found.");
+            goto("/"); // 세션 ID가 없을 경우 리다이렉트
+            return;
+        }
+
+        for (const _url of urls) {
+            try {
+                const response = await fetch(`${_url}?session_id=${sessionId}`);
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+
+                const result = await response.json();
+                console.log("API Response:", result);
+
+                if (result.success) {
+                    console.log("Session valid");
+                } else {
+                    console.error("Session invalid:", result);
+                    goto("/"); // 유효하지 않은 세션일 경우 리다이렉트
+                }
+            } catch (error) {
+                console.error("Error during API request:", error);
+                goto("/"); // 에러 발생 시 리다이렉트
+            }
+        }
+    }
+
+    // 페이지 로드 시 세션 확인 실행
+    checkSessions();
+</script>
+
+<button on:click={() => history.back()}>👈🏻 Go Back</button>
+<button on:click={() => window.location.href="/"}>Home</button>
+
+<slot />
--- src/routes/apps/stock_database/+page.svelte
+++ src/routes/apps/stock_database/+page.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
-    import { goto } from '$app/navigation';
-
     let tickers: any;
-
-    // API URL 리스트
-    const urls: string[] = [
-        "https://y0rfa1se.duckdns.org/api/database/session/check",
-    ];
 
     // 쿠키에서 세션 ID 가져오기
     const cookies = document.cookie;
@@ -21,41 +14,11 @@
 
     // 비동기 함수로 API 호출 및 처리
     async function checkSessions() {
-        if (!sessionId) {
-            console.error("Session ID not found.");
-            goto("/"); // 세션 ID가 없을 경우 리다이렉트
-            return;
-        }
+        const url = "https://y0rfa1se.duckdns.org/api/database/stock/tickers?session_id=" + sessionId;
+        const response = await fetch(url);
+        const result = await response.json();
 
-        for (const _url of urls) {
-            try {
-                const response = await fetch(`${_url}?session_id=${sessionId}`);
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-
-                const result = await response.json();
-                console.log("API Response:", result);
-
-                if (result.success) {
-                    console.log("Session valid");
-
-                    const url = "https://y0rfa1se.duckdns.org/api/database/stock/tickers?session_id=" + sessionId;
-                    const response = await fetch(url);
-                    const result = await response.json();
-
-                    tickers = result;
-
-                    console.log(tickers);
-                } else {
-                    console.error("Session invalid:", result);
-                    goto("/"); // 유효하지 않은 세션일 경우 리다이렉트
-                }
-            } catch (error) {
-                console.error("Error during API request:", error);
-                goto("/"); // 에러 발생 시 리다이렉트
-            }
-        }
+        tickers = result;
     }
 
     // 페이지 로드 시 세션 확인 실행
