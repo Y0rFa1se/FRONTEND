b197d8501d00d647eff0bff0aafb6ef1decf1857
Y0rFa1se <neargul123@gmail.com>
2025-03-08 10:31:56 +0900

    zxcv

--- src/routes/apps/stock_database/+page.svelte
+++ src/routes/apps/stock_database/+page.svelte
@@ -44,7 +44,7 @@
                     const response = await fetch(url);
                     const result = await response.json();
 
-                    tickers = result.data.data;
+                    tickers = result.data;
 
                     console.log(tickers);
                 } else {
